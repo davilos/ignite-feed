0042b00ae8cbcc57e692dcc6d75fe66a3fd8a4e3
Dávilos <daviloscostagg@hotmail.com>
2025-03-01 19:15:13 -0300

    feat: implementa componente Avatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,5 +1,11 @@
+/* eslint-disable react/prop-types */
 import styles from "./Avatar.module.css";
 
-export function Avatar(props) {
-  return <img className={styles.avatar} src={props.src} />;
+export function Avatar({ hasBorder = true, src }) {
+  return (
+    <img
+      className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+      src={src}
+    />
+  );
 }
