08394e50ffaf6cf706508d6c14c82f5e059515e5
Dávilos <daviloscostagg@hotmail.com>
2025-03-01 21:56:00 -0300

    feat: cria um array de posts

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,57 @@ import { Sidebar } from "./components/Sidebar";
 import styles from "./App.module.css";
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2025-02-24 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator @Rocketseat",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2025-02-25 20:00:00"),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -14,11 +65,16 @@ function App() {
         <Sidebar />
 
         <main>
-          <Post
-            author="Diego Fernandes"
-            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, voluptas! Ipsam fuga adipisci distinctio nemo atque ad eos totam reiciendis. Fugit eligendi provident, reiciendis quos a fuga vel ullam repudiandae."
-          />
-          <Post author="Gabriel Buzzi" content="Um novo post!" />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
