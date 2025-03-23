80e12f0716ee57718827e23f4c8de175aabae75e
Dávilos <daviloscostagg@hotmail.com>
2025-03-23 20:44:19 -0300

    feat: add new handlers and states

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,21 @@
 import { ThumbsUp, Trash } from "@phosphor-icons/react";
 import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
+import { useState } from "react";
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleAddLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/davilos.png" />
@@ -20,7 +33,7 @@ export function Comment({ content }) {
               </time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
@@ -29,9 +42,9 @@ export function Comment({ content }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleAddLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
