11aebd002cd2cb0e23710383b2d1e5d883c47967
Dávilos <daviloscostagg@hotmail.com>
2025-03-25 01:05:15 -0300

    refactor: add the Content type to the const

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Header } from "./components/Header";
-import { Post } from "./components/Post";
-import { Sidebar } from "./components/Sidebar";
+import { Header } from "./components/Header/Header";
+import { Content, Post } from "./components/Post/Post";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 
 import styles from "./App.module.css";
 import "./global.css";
@@ -15,19 +15,19 @@ const posts = [
     },
     content: [
       {
-        type: "paragraph" as const,
+        type: "paragraph",
         content: "Fala galeraa 👋",
       },
       {
-        type: "paragraph" as const,
+        type: "paragraph",
         content:
           "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
       },
       {
-        type: "link" as const,
+        type: "link",
         content: "jane.design/doctorcare",
       },
-    ],
+    ] as Content,
     publishedAt: new Date("2025-02-24 20:00:00"),
   },
   {
@@ -39,19 +39,19 @@ const posts = [
     },
     content: [
       {
-        type: "paragraph" as const,
+        type: "paragraph",
         content: "Fala galeraa 👋",
       },
       {
-        type: "paragraph" as const,
+        type: "paragraph",
         content:
-          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+          "Acabei de sub  as constir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
       },
       {
-        type: "link" as const,
+        type: "link",
         content: "jane.design/doctorcare",
       },
-    ],
+    ] as Content,
     publishedAt: new Date("2025-02-25 20:00:00"),
   },
 ];
